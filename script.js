ff5a6c451990f803461b4e9dc20a32731e3190e9
unknown <nisargshah2113@gmail.com>
2018-10-08 13:01:19 +0530

    Message for others

---
+++ script.js
@@ -0,0 +1,72 @@
+/*
+Functions
+function sayHello() {
+    console.log("Hello");
+}
+
+sayHello();
+
+var sayBye = function() {
+    console.log("Bye");
+}
+
+sayBye();
+
+function sing(song){
+    return song;
+}
+
+var ly = sing ("HIIIIIIIIIIIIIIIII");
+alert(ly);*/
+
+/*
+Arrays
+var list = [
+    ["tiger", "cat", "dog", "bird"]
+];
+console.log(list[0][1]);
+
+list.push()
+list.pop()
+Other on w3 schools
+
+var list_objects = [
+    {
+        name: "Nisarg",
+        password: "134"
+        gender: male
+    },
+    {
+        name: "N",
+        password: "143"
+        gender: female
+    }
+];
+*/
+
+/*
+Objects : Property and Value
+
+var user = {
+    name: "John",
+    age: 34,
+    hobby: football,
+    spells: ["hi", "hey", "hello"]//array inside object
+    shout: function(){
+        console.log("Hiiiiiiii");//functions inside object
+    } 
+}
+
+ToGrab
+user.name
+user.age
+
+Add properties
+user.favouriteFood = "Pizza";
+user.age = 40;
+*/
+
+/*
+null data type
+var nullObj = null;
+*/
